1a6a65cc02d3301b2a1abaa623e35f247ad1f474
Francisco E. Paez <franpaez+github@gmail.com>
2022-10-27 15:46:09 -0300

    Agrega funciones wrap solo si el target lo requiere.

--- libs/mbed/funcs.cpp
+++ libs/mbed/funcs.cpp
@@ -2,8 +2,11 @@
 
 extern "C" pid_t _getpid();
 extern "C" void _kill();
+
+#ifdef STM32F746xx
 extern "C" int __wrap_vsnprintf (char * s, size_t n, const char * format, va_list arg );
 extern "C" int __wrap_vfprintf ( FILE * stream, const char * format, va_list arg );
+#endif
 
 pid_t _getpid(void)
 {
@@ -15,6 +18,7 @@ void _kill(void)
 	;
 }
 
+#ifdef STM32F746xx
 int __wrap_vsnprintf (char * s, size_t n, const char * format, va_list arg )
 {
 	return 0;
@@ -24,3 +28,4 @@ int __wrap_vfprintf ( FILE * stream, const char * format, va_list arg )
 {
 	return 0;
 }
+#endif
