fe2e7aa07fb0d826bc0c4ac10976d033f6f3152b
Francisco E. Páez <franpaez+github@gmail.com>
2022-11-03 17:55:13 -0300

    Agrega wrappers directamente en variable LD_FLAGS

--- libs/slack/module.mk
+++ libs/slack/module.mk
@@ -7,13 +7,13 @@ INCLUDE_PATHS += -I./libs/slack/$(FREERTOS_VERSION)
 
 # Replace these functions
 ifeq ($(FREERTOS_VERSION), 10.0.1)
-WRAP = -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
+LD_FLAGS += -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
 endif
 ifeq ($(FREERTOS_VERSION), 10.4.1)
-WRAP = -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
+LD_FLAGS += -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
 endif
 ifeq ($(FREERTOS_VERSION), 10.4.6)
-WRAP = -Wl,--wrap=xTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
+LD_FLAGS += -Wl,--wrap=xTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
 endif
 
 endif
